6c3ad2b5c956d0f07dc2d9f18faa26cdd8a95c35
eboueleamana <chriscome04@gmail.com>
2023-06-26 23:12:08 +0100

    mettre en ligne

--- pizzamama/pizzamama/settings.py
+++ pizzamama/pizzamama/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c1l5#i#*#ez2-v&g0!_u^98c1dz#$mt7rvglyv%teq5_pvl2ch
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["eacc.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
